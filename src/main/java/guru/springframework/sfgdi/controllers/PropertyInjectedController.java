39dfc423821dd22a891a365f2b38decb03b19de5
Marc <marc.marechal@dell.com>
2020-12-01 22:15:29 +0800

    3 types of injection

---
+++ src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgdi.controllers;
+
+
+import guru.springframework.sfgdi.services.GreetingService;
+
+public class PropertyInjectedController {
+
+    GreetingService greetingService;
+
+    public String getGreeting() {
+        return greetingService.sayHello();
+    }
+
+
+}
