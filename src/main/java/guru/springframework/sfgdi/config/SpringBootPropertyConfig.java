730c2a6b2e3b297e0df05230be2d41a1649491c0
Marc <marc.marechal@dell.com>
2020-12-12 21:21:24 +0800

    get properties from SpringBoot prop file

--- src/main/java/guru/springframework/sfgdi/config/SpringBootPropertyConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/SpringBootPropertyConfig.java
@@ -2,23 +2,15 @@ package guru.springframework.sfgdi.config;
 
 import guru.springframework.sfgdi.examplebeans.FakeDataSource;
 import guru.springframework.sfgdi.examplebeans.FakeJmsSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
-@Profile("legacy")
+@Profile("spring")
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms-broker.properties"})
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms-broker.properties")
-})
-public class PropertyConfig {
-
-    @Autowired
-    Environment env;
+public class SpringBootPropertyConfig {
 
     @Value("${guru.username}")
     String user;
@@ -42,7 +34,7 @@ public class PropertyConfig {
     FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
         fakeDataSource.setUsername(user);
-        fakeDataSource.setPassword(env.getProperty("PASSWORD"));
+        fakeDataSource.setPassword(password);
         fakeDataSource.setDburl(dburl);
         return fakeDataSource;
     }
@@ -54,9 +46,5 @@ public class PropertyConfig {
         fakeJmsSource.setPassword(jmsPassword);
         fakeJmsSource.setJmsUrl(jmsUrl);
         return fakeJmsSource;
-    }
-
-    @Bean static PropertySourcesPlaceholderConfigurer properties(){
-        return new PropertySourcesPlaceholderConfigurer();
     }
 }
